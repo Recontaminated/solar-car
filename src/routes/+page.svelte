7d2b8eee5ef8286e8f563b7630f110cd12ac527c
Recontaminated <69064548+Recontaminated@users.noreply.github.com>
2023-06-04 20:36:55 -0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<div class="main">
+
+    <h1>Welcome to the 15 Minute Picture Challenge</h1>
+
+</div>
+
+
+<style lang="scss">
+    .main{
+        background-image: linear-gradient(to top, rgba(0,0,0,1),rgba(0,0,0,.5)), url(https://images.duckarmada.com/1oUGYbBfzRjW/direct.jpg);
+      height: calc(100vh + 100px);
+      -webkit-background-size: cover;
+      background-size: cover;
+      background-repeat: no-repeat;
+    }
+</style>
